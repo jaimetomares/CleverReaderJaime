2b8fb7336c47d26524e3de72aa61595dbca312a7
Jaime <jimmybaq@gmail.com>
2022-11-26 16:39:53 +0100

    summary app

--- summary/summary/asgi.py
+++ summary/summary/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for summary_service project.
+ASGI config for summary project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'summary_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'summary.settings')
 
 application = get_asgi_application()
--- summary/summary_app/urls.py
+++ summary/summary_app/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-
 from . import views
-
 urlpatterns = [
-    path('file', views.consume_file, name='consume_file')
+    path('', views.index, name = 'index')
 ]

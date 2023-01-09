14bbdf49970ba840f4218fe05f52523cd409160d
avendo <leon.kranjcevic@outlook.com>
2023-01-09 20:17:23 +0100

    Add connection to middleware

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
+import json
+
+import requests
 from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
-from django.conf import settings
 import PyPDF2
 import re
 import concurrent.futures
@@ -9,9 +11,9 @@ from unidecode import unidecode
 
 
 def consume_file(request):
-    if request.method == 'POST':
-        
-        file = request.FILES['file']
+    if request.method == 'GET':
+        resource_id = request.GET['id']
+        file = requests.get(f"https://clever-reader-middleware.onrender.com/api/v1/file/{resource_id}")
 
         # Verifica si la extensión del archivo es '.pdf'
         if not file.name.endswith('.pdf'):
@@ -30,13 +32,11 @@ def consume_file(request):
         openai.api_key = "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"
         model_engine = "text-davinci-003"
 
-
         # Get the number of pages in the PDF
         pages = doc.getNumPages()
 
         # Initialize an empty string to store the text
         text = ""
-
 
         # Iterate through each page in the PDF
         for i in range(pages):
@@ -56,20 +56,18 @@ def consume_file(request):
             text = unidecode(text)
 
             text += curr_text
-        
+
         # Delete the reference sections from the text
         text = re.sub(r'References.*', '', text, flags=re.DOTALL)
- 
+
         # Delete tables from the text
         text = re.sub(r'\n\s*\n\s*\|.*\|\s*\n', '\n', text, flags=re.DOTALL)
- 
 
         # Define a pattern for URLs
         pattern = r'(http|https).+?(?=\s|$)'
         # Use re.sub() to search for the pattern and replace it with an empty string
         text = re.sub(pattern, '', text)
 
- 
         # Replace curly quotes with straight quotes
         text = re.sub("’", "'", text)
         # Replace any non-alphanumeric, non-quote, non-colon, non-semicolon, non-period, non-exclamation, non-question mark characters with a space
@@ -78,14 +76,13 @@ def consume_file(request):
         # Remove empty parentheses
         text = re.sub('()', '', text)
         # Remove bracketed numbers
-        text = re.sub(r'\[[0-9]*\]', ' ', text)  
+        text = re.sub(r'\[[0-9]*\]', ' ', text)
         # Replace multiple spaces with a single space
         text = re.sub(r'\s+', ' ', text)
         # Replace curly quotes with straight quotes
         text = re.sub("’", "'", text)
         # Replace any non-alphanumeric, non-quote characters with a space
         text = re.sub("[^a-zA-Z' ]+", " ", text)
-
 
         # Remove the word 'Fig' from the text
         text = re.sub('Fig', '', text)
@@ -96,8 +93,6 @@ def consume_file(request):
         # Remove the word 'Page' from the text
         text = re.sub('Page', '', text)
 
-
-
         # Initialize an empty string to store the summary
         res = ""
 
@@ -105,20 +100,20 @@ def consume_file(request):
         summary = "Summarize the following text and group it by its content, ignoring the unsense phrases \n"
 
         # Divide the text into chunks of x characters
-        context_parts = [(summary + "Fragment: " + str(i) + "\n" + text[i:i+4500]) for i in range(0, len(text), 4500)]
+        context_parts = [(summary + "Fragment: " + str(i) + "\n" + text[i:i + 4500]) for i in range(0, len(text), 4500)]
 
         # Create a ThreadPoolExecutor with the same number of threads as the number of processors in your machine
         with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Create a list of futures with the threads that will perform the summary of each fragment
-            futures = [executor.submit(lambda context_part: openai.Completion.create(engine=model_engine, prompt=context_part, max_tokens=160, n=1,stop=None,temperature=0.75, top_p=1, frequency_penalty=0, presence_penalty=0).choices[0].text, context_part) for context_part in context_parts]
+            # Create a list of futures with the threads that will perform the summary of each fragment
+            futures = [executor.submit(lambda context_part:
+                                       openai.Completion.create(engine=model_engine, prompt=context_part,
+                                                                max_tokens=160, n=1, stop=None, temperature=0.75,
+                                                                top_p=1, frequency_penalty=0,
+                                                                presence_penalty=0).choices[0].text, context_part) for
+                       context_part in context_parts]
 
             # Iterate through the list of futures and collect the result of each one
             for future in concurrent.futures.as_completed(futures):
                 res += future.result()
-
-        # Return the summary as an HTTP response
-        return HttpResponse(res)
-
-
-       
-
+        response_data = {'data': res}
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
